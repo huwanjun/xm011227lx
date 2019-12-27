5ffc841c7922555b3b163fdca184c647ac9a59d8
1705ahwj <994632322@qq.com>
2019-12-27 21:08:06 +0800

    第二次提交

--- app/src/main/java/com/bawei/xm011227lx/model/bean/Bean.java
+++ app/src/main/java/com/bawei/xm011227lx/model/bean/Bean.java
@@ -1,5 +1,7 @@
 package com.bawei.xm011227lx.model.bean;
 
+import com.stx.xhb.xbanner.entity.SimpleBannerInfo;
+
 import java.util.List;
 
 /**
@@ -11,7 +13,7 @@ public class Bean {
 
 
     /**
-     * result : [{"commodityId":140,"commodityName":"小白鞋男时尚新款运动休闲男鞋韩版潮流厚底原宿风板鞋","masterPic":"http://172.17.8.100/images/small/commodity/nx/nbx/6/1.jpg","price":398,"saleNum":0},{"commodityId":137,"commodityName":"唐狮冬季男士冬休闲鞋高帮男鞋男士板鞋休闲男鞋子高帮男鞋百搭休闲板鞋男","masterPic":"http://172.17.8.100/images/small/commodity/nx/nbx/3/1.jpg","price":79,"saleNum":3},{"commodityId":145,"commodityName":"如熙新款男鞋韩版学生时尚百搭潮流低帮男帆布鞋简约英伦风舒适男板鞋轻便耐磨系带潮鞋","masterPic":"http://172.17.8.100/images/small/commodity/nx/nfbx/4/1.jpg","price":119,"saleNum":3},{"commodityId":139,"commodityName":"秋季男鞋简约百搭男小白鞋韩版潮流男板鞋跑步鞋子ins超火的运动鞋学生时尚嘻哈潮鞋","masterPic":"http://172.17.8.100/images/small/commodity/nx/nbx/5/1.jpg","price":109,"saleNum":0},{"commodityId":136,"commodityName":"板鞋休闲鞋男男士休闲运动鞋男士鞋子休闲皮鞋男士休闲鞋男鞋","masterPic":"http://172.17.8.100/images/small/commodity/nx/nbx/2/1.jpg","price":99,"saleNum":0}]
+     * result : [{"imageUrl":"http://172.17.8.100/images/small/banner/cj.png","jumpUrl":"http://172.17.8.100/htm/lottery/index.html","rank":5,"title":"抽奖"},{"imageUrl":"http://172.17.8.100/images/small/banner/hzp.png","jumpUrl":"wd://commodity_list?arg=1001007005","rank":5,"title":"美妆工具"},{"imageUrl":"http://172.17.8.100/images/small/banner/lyq.png","jumpUrl":"wd://commodity_info?arg=83","rank":5,"title":"连衣裙"},{"imageUrl":"http://172.17.8.100/images/small/banner/px.png","jumpUrl":"wd://commodity_info?arg=165","rank":5,"title":"跑鞋"},{"imageUrl":"http://172.17.8.100/images/small/banner/wy.png","jumpUrl":"wd://commodity_list?arg=1001002004","rank":5,"title":"卫衣"}]
      * message : 查询成功
      * status : 0000
      */
@@ -44,59 +46,54 @@ public class Bean {
         this.result = result;
     }
 
-    public static class ResultBean {
+    public static class ResultBean extends SimpleBannerInfo {
         /**
-         * commodityId : 140
-         * commodityName : 小白鞋男时尚新款运动休闲男鞋韩版潮流厚底原宿风板鞋
-         * masterPic : http://172.17.8.100/images/small/commodity/nx/nbx/6/1.jpg
-         * price : 398
-         * saleNum : 0
+         * imageUrl : http://172.17.8.100/images/small/banner/cj.png
+         * jumpUrl : http://172.17.8.100/htm/lottery/index.html
+         * rank : 5
+         * title : 抽奖
          */
 
-        private int commodityId;
-        private String commodityName;
-        private String masterPic;
-        private int price;
-        private int saleNum;
+        private String imageUrl;
+        private String jumpUrl;
+        private int rank;
+        private String title;
 
-        public int getCommodityId() {
-            return commodityId;
+        public String getImageUrl() {
+            return imageUrl;
         }
 
-        public void setCommodityId(int commodityId) {
-            this.commodityId = commodityId;
+        public void setImageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
         }
 
-        public String getCommodityName() {
-            return commodityName;
+        public String getJumpUrl() {
+            return jumpUrl;
         }
 
-        public void setCommodityName(String commodityName) {
-            this.commodityName = commodityName;
+        public void setJumpUrl(String jumpUrl) {
+            this.jumpUrl = jumpUrl;
         }
 
-        public String getMasterPic() {
-            return masterPic;
+        public int getRank() {
+            return rank;
         }
 
-        public void setMasterPic(String masterPic) {
-            this.masterPic = masterPic;
+        public void setRank(int rank) {
+            this.rank = rank;
         }
 
-        public int getPrice() {
-            return price;
+        public String getTitle() {
+            return title;
         }
 
-        public void setPrice(int price) {
-            this.price = price;
+        public void setTitle(String title) {
+            this.title = title;
         }
 
-        public int getSaleNum() {
-            return saleNum;
-        }
-
-        public void setSaleNum(int saleNum) {
-            this.saleNum = saleNum;
+        @Override
+        public Object getXBannerUrl() {
+            return null;
         }
     }
 }
